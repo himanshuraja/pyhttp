e0ed3e76cd882ee10b9ceaf4e15a009fbfb53f71
himanshu <himanshusharmahs974@gmail.com>
2016-05-20 10:05:47 +0530

    webserver in python

---
+++ webserver.py
@@ -0,0 +1,14 @@
+import SimpleHTTPServer,SocketServer,sys
+PORT=sys.argv[1]
+def runserver():
+  try:
+   httphandler=SimpleHTTPServer.SimpleHTTPRequestHandler
+   httpd=SocketServer.TCPServer(("192.168.1.6 ",int(PORT)),httphandler)
+   print "python web server serving at port," +PORT
+   httpd.serve_forever()
+  except (KeyboardInterrupt,SystemExit):
+   print "exiting.."
+   sys.exit
+  except:
+   print "THERE WAS A PROBLEM STARTING THE WEBSERVER"
+runserver()
